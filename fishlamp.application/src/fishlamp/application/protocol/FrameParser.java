65a41399756f0952ca2c1d1064fbc6e0815f1508
g_balage <balage.g@gmail.com>
2011-11-17 19:53:43 +0100

    PC-side app

---
+++ fishlamp.application/src/fishlamp/application/protocol/FrameParser.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package fishlamp.application.protocol;
+
+/**
+ * @author balazs.grill
+ *
+ */
+public abstract class FrameParser {
+
+	public void newByte(short d){
+		
+	}
+	
+}
---
+++ fishlamp.application/src/fishlamp/application/protocol/ProtocolParser.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fishlamp.application.protocol;
+
+/**
+ * @author balazs.grill
+ *
+ */
+public class ProtocolParser {
+
+	private FrameParser fparser = null;
+	
+	public void newByte(short d){
+		if (fparser == null){
+			
+		}
+	}
+	
+}
